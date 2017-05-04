0726e2ed7bb4ffb963f5769fe1ffe6b918e7c51d
Douglas Lehr <dllehr@us.ibm.com>
2017-05-04 16:03:38 -0500

    scripts: move script files to separate folder
    Move nat.sh patch_source.sh and setup_git_repo.sh to scripts
    directory for easier maintenance

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,18 @@
-#execute deployer
+# Copyright 2017 IBM Corp.
+#
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 ACTIVATE_FILE=".activate"
 PLAYBOOK_LOC="playbooks/master_playbook.yml"
 SETUP_ENV_LOC="scripts/setup-env"
@@ -24,6 +38,7 @@ ansible-playbook -i hosts lxc-create.yml -K && ansible-playbook -i hosts install
 #cd ../../
 #ansible-playbook -i $DYNAMIC_INVENTORY playbooks/post_os_wait.yml
 #cd ${GENESIS_FULL}/playbooks
+
 echo "Wait 15 minutes"
 sleep 15m
 #Cluster Node Networking
@@ -35,7 +50,7 @@ ansible-playbook -i $DYNAMIC_INVENTORY configure_operating_systems.yml
 cd ../../
 
 ssh -i ~/.ssh/id_rsa_ansible-generated deployer@192.168.3.2 'sudo apt-get update && sudo apt-get install -y iptables'
-ssh -i ~/.ssh/id_rsa_ansible-generated deployer@192.168.3.2 'sudo bash -s' < nat.sh
+ssh -i ~/.ssh/id_rsa_ansible-generated deployer@192.168.3.2 'sudo bash -s' < scripts/nat.sh
 
 #call PowerAI playbook
 ansible-playbook -i $DYNAMIC_INVENTORY $PLAYBOOK_LOC
--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
+# Copyright 2017 IBM Corp.
+#
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -e
 GENESIS_REMOTE="https://github.com/open-power-ref-design/cluster-genesis.git"
 GENESIS_LOCAL="cluster-genesis"
@@ -44,10 +60,10 @@ fi
 #sudo apt-get install aptitude
 
 #pull cluster-genesis into project directory
-./setup_git_repo.sh "${GENESIS_REMOTE}" "${GENESIS_LOCAL}" "${GENESIS_COMMIT}"
+./scripts/setup_git_repo.sh "${GENESIS_REMOTE}" "${GENESIS_LOCAL}" "${GENESIS_COMMIT}"
 
 #apply any patches to genesis.
-./patch_source.sh "${GENESIS_LOCAL}"
+./scripts/patch_source.sh "${GENESIS_LOCAL}"
 
 #sed -i /sources.list/s/^/#/ ${GENESIS_LOCAL}/os_images/config/*.seed
 
